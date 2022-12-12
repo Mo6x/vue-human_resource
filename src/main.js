6b4e85cd809edaadb99dc9136ffa7cd23aee4191
christopher-moses <christmoses1999@icloud.com>
2022-12-13 00:03:07 +0100

    Configuring Vuex Store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from 'axios'
+import Vuex from "vuex";
 import vueAxios from 'vue-axios'
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
